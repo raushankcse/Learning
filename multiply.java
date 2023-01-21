6aca53b4f747c36ee2511f2166db41dd1961235a
Raushan Kumar <prks995@gmail.com>
2023-01-21 15:16:08 +0530

    symbol change in code

--- multiply.java
+++ multiply.java
@@ -2,7 +2,7 @@ public class multiply{
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-        System.out.println("Sum: "+a+b);
+        System.out.println("Sum: "+a*b);
     }
 
 } 
